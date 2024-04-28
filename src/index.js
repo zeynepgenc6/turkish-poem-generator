63b3449d114d9fca0839213ff63dd98055c3caaf
zeynepgenc6 <zeynepgenc6@gmail.com>
2024-04-28 04:59:02 -0500

    added element in css html and js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ function generatePoem(event) {
   let context =
     "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning ";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class="generating">⏳ Generating a Turkish poem about ${instructionsInput.value}</div>`;
