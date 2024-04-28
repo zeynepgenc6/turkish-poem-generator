e363c40322ca410bc63369aa4bef8c0011d0bc3a
zeynepgenc6 <zeynepgenc6@gmail.com>
2024-04-28 04:23:23 -0500

    added axios in html file and added ai, fuctions in js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-function genaratePoem(event){
-    event.preventDefault();
-    
-    new Typewriter("#poem", {
-      strings: "İkimiz birden sevinebiliriz göğe bakalım",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
-    
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "tbde3214cf01bode24a7a0423fab97ef";
+  let prompt = `User instructions: Generate a Turkish poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Turkish poem about ${instructionsInput.value}</div>`;
+  axios.get(apiUrl).then(displayPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", genaratePoem);
+poemFormElement.addEventListener("submit", generatePoem);
